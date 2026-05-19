76caae2bd4d4019e9af894701a11942dc1c0f273
Mani Amar Sehgal <sehgalmaniamar@gmail.com>
2026-05-19 08:22:48 +0530

    color change

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default function SectionHeading({ title, subtitle }) {
   return (
     <div className="text-center">
-      <h2 className="text-3xl font-semibold text-gray-800 dark:text-gray-100 mb-2">
+      <h2 className="text-3xl font-semibold text-gray-800 dark:text-gray-500 mb-2">
         {title}
       </h2>
       {subtitle && (
-        <p className="text-gray-700 dark:text-gray-300 text-lg">{subtitle}</p>
+        <p className="text-gray-900 dark:text-gray-400 text-lg">{subtitle}</p>
       )}
     </div>
   );
